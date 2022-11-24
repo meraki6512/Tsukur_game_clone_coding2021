b88eef28960251a9a9c6eb8e9c58d3c4261a5a10
meraki6512 <jweun1@soongsil.ac.kr>
2022-11-25 08:04:44 +0900

    Add files via upload

---
+++ Assets/Scrpit/TestDialogue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDialogue : MonoBehaviour {
+    [SerializeField]
+    public Dialogue dialogue;
+
+    private DialogueManager theDM;
+
+	// Use this for initialization
+	void Start () {
+        theDM = FindObjectOfType<DialogueManager>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision) //boxcollider에 충돌할 때 인식
+    {
+        if(collision.gameObject.name == "Player") //출동한 대상이 Player일 경우 실행
+        {
+            theDM.ShowDialogue(dialogue);
+        }
+    }
+
+}
